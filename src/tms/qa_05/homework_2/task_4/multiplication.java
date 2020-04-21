a49c52831511c9e76c9ba873ff18addbb656c18d
Aleksandr Fomin <RezviyBelorus@gmail.com>
2020-04-21 21:54:43 +0300

    Update multiplication.java

--- src/tms/qa_05/homework_2/task_4/multiplication.java
+++ src/tms/qa_05/homework_2/task_4/multiplication.java
@@ -1,14 +1,15 @@
 package tms.qa_05.homework_2.task_4;
 
+// multiplication -> Multiplication
 public class multiplication {
     public static void main(String[] args) {
         int A = 2;
         int B = -5;
         int sum = 0;
-        for (int x = 0, y = 0; x < B; x++, y--) {
+        for (int x = 0, y = 0; x < B; x++, y--) { // зачем тебе переменная х и у? можно без них
             sum += A;
-
-
+// пустая строка не нужна
+// пустая строка не нужна
         }
         System.out.println(sum);
     }
