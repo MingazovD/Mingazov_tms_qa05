3e216249964076ea396db57594efeef457689685
Aleksandr Fomin <RezviyBelorus@gmail.com>
2020-04-21 21:55:42 +0300

    Update inch.java

--- src/tms/qa_05/homework_2/task_5/inch.java
+++ src/tms/qa_05/homework_2/task_5/inch.java
@@ -1,14 +1,15 @@
 package tms.qa_05.homework_2.task_5;
 
+// inch -> Inch
 public class inch {
     public static void main(String[] args) {
-        double num_1 = 2.54;
+        double num_1 = 2.54; // так себе названия переменных
         double num_2 = 1;
         for (int num_3 = 1; num_3 <= 20; num_3++) {
             num_2 = num_3 * num_1;
 
             System.out.println(num_3 + " inch = " + num_2 + " centimeter ");
-
+// пустая строка не нужна
         }
     }
 }
