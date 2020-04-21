a4b9e71ebde5488cd9ecea3d1123fd35cbcd876c
Aleksandr Fomin <RezviyBelorus@gmail.com>
2020-04-21 21:50:47 +0300

    Update Amoeba.java

--- src/tms/qa_05/homework_2/task_2/Amoeba.java
+++ src/tms/qa_05/homework_2/task_2/Amoeba.java
@@ -4,7 +4,10 @@ public class Amoeba {
     public static void main(String[] args) {
         int beginAmoebas = 1;
         int allAmoebas = 0;
+        // 25 в отдельную переменную
+        // 3 в отдельную переменную
         for (int hour = 3; hour < 25; hour += 3) {
+            // 2 в отдельную переменную
             allAmoebas = beginAmoebas *= 2;
 
             System.out.println("через  " + hour + " часа амеб будет " + allAmoebas);
