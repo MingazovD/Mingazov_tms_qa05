f76f846d1c8099326f811c1c4ad581ead793a24d
Aleksandr Fomin <RezviyBelorus@gmail.com>
2020-04-21 21:51:09 +0300

    Update arithmeticProgression.java

--- src/tms/qa_05/homework_2/task_3/arithmeticProgression.java
+++ src/tms/qa_05/homework_2/task_3/arithmeticProgression.java
@@ -4,6 +4,7 @@ public class arithmeticProgression {
     public static void main(String[] args) {
         int num_1 = 1;
         int sum = 0;
+        // 256 в отдельную переменную
         while (num_1 <= 256) {
             sum += num_1;
             num_1 *= 2;
