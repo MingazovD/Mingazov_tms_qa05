44dae8ed04bc30b54b7ab0c697579263f25adb44
Aleksandr Fomin <RezviyBelorus@gmail.com>
2020-04-24 10:36:52 +0300

    Update ArrayOdd.java

--- src/tms/qa_05/homework_3/task_3/ArrayOdd.java
+++ src/tms/qa_05/homework_3/task_3/ArrayOdd.java
@@ -4,7 +4,8 @@ public class ArrayOdd {
     public static void main(String[] args) {
         int[] array = {5, 10, 15, 6};
         for (int i = 0; i < array.length; i++) {
-            if (array[i] % 2 != 0) ;
+            if (array[i] % 2 != 0) ; // у тебя в конце строки стоит ';', таким образом получается, что в операторе IF ничего не происходит
+            // if () {тело оператора 'if'} - фигурные скобки очень важны
             array[i] = 0;
             {
                 //  не работает, не понимаю куда посмотреть
