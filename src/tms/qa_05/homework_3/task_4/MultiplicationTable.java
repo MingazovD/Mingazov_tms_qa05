f6cee9c426a906f9afd74a4ad6782c76524f5f60
Aleksandr Fomin <RezviyBelorus@gmail.com>
2020-04-24 10:38:47 +0300

    Update MultiplicationTable.java

--- src/tms/qa_05/homework_3/task_4/MultiplicationTable.java
+++ src/tms/qa_05/homework_3/task_4/MultiplicationTable.java
@@ -2,17 +2,18 @@ package tms.qa_05.homework_3.task_4;
 
 import java.util.Arrays;
 
+// не забывай форматировать код
 public class MultiplicationTable {
     public static void main(String[] args) {
-        int[][] multiplicationTable = new int[10][2];
+        int[][] multiplicationTable = new int[10][2]; // 10х2 разве хватит для всей таблицы умножени?
 
         for (int i = 0; i < multiplicationTable.length; i++) {
             for (int j = 0; j < multiplicationTable[i].length; j++) {
                 multiplicationTable[i][j] = i + j  ;
-
+// пустая строка не нужна
             }
-
-            System.out.println(Arrays.toString(multiplicationTable[i]));
+// пустая строка не нужна
+            System.out.println(Arrays.toString(multiplicationTable[i])); // логику печати лучше вынести за пределы этих циклов
         }
 }
 }
